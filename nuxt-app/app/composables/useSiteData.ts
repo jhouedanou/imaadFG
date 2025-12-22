79129111d1d584ae0972bbeaadf07e87bae58fea
Jean Luc Houédanou <jhouedanou@gmail.com>
2025-12-22 17:53:11 +0000

    Expose images in useSiteData composable
    Added the 'images' property from site-data.json to the returned object in useSiteData, making image data accessible to consumers of this composable.

--- nuxt-app/app/composables/useSiteData.ts
+++ nuxt-app/app/composables/useSiteData.ts
@@ -3,6 +3,7 @@ import siteData from '../data/site-data.json'
 export const useSiteData = () => {
   return {
     site: siteData.site,
+    images: siteData.images,
     navigation: siteData.navigation,
     languages: siteData.languages,
     pages: siteData.pages
